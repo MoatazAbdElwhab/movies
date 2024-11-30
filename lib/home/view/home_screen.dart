b29f0a591889b7028d48e69c4facf3e0dc989d35
ASHmd97 <ahmed.hmd1996@gmail.com>
2024-11-30 04:37:20 +0200

    edit theme

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:movies/details/view/screens/movie_details.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   static const routeName = '/';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MovieDetails();
   }
 }
