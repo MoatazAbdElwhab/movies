1ef5f3fd2fd22d5583a122e1c60208ae11c53056
mohammed hussein <muhammedhussein739@gmail.com>
2024-12-02 22:10:44 +0200

    edit error indicator.

--- lib/shared/widgets/error_indicator.dart
+++ lib/shared/widgets/error_indicator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ErrorIndicator extends StatelessWidget {
   final String errorMessage;
@@ -10,8 +11,12 @@ class ErrorIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(errorMessage),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 12.w),
+      child: Text(
+        errorMessage,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
     );
   }
 }
